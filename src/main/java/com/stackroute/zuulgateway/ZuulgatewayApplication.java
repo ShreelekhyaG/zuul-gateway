a08a79a7914b21e35b48431494062d481d75a0fb
ShreelekhyaG <shreelu_gampa@yahoo.com>
2018-10-15 10:39:23 +0530

    checked with movie-service2

--- src/main/java/com/stackroute/zuulgateway/ZuulgatewayApplication.java
+++ src/main/java/com/stackroute/zuulgateway/ZuulgatewayApplication.java
@@ -7,13 +7,11 @@ import filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class ZuulgatewayApplication {
 
 	public static void main(String[] args) {
